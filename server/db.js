940477cf9eb236cc63328344bdeb505e049a910e
Dhiraj chowdhury <dhirajchowdhury001@gmail.com>
2026-04-30 20:31:57 +0530

    finals 01

--- server/db.js
+++ server/db.js
@@ -1,7 +1,26 @@
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// pool.connect()
+//   .then(client => {
+//     console.log("✅ Connected to Supabase DB");
+//     client.release();
+//   })
+//   .catch(err => {
+//     console.error("❌ DB Connection Error:", err.message);
+//   });
+
+// module.exports = pool;
 const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect()
